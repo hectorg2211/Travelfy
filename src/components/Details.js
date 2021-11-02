f9eed87fe24c22494b3835519a8c327d0fa44299
hectorg2211 <hector032716@gmail.com>
2021-11-02 11:02:19 -0700

    Added detail images

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,5 +1,34 @@
 import React from "react";
 
-const Details = () => <div>Details</div>;
+const Details = () => (
+  <div className="details">
+    <div className="detail-imgs">
+      <img
+        src="../assets/hotel-1.jpg"
+        alt="hotel 1"
+        className="detail-imgs__img"
+      />
+      <img
+        src="../assets/hotel-2.jpg"
+        alt="hotel 1"
+        className="detail-imgs__img"
+      />
+      <img
+        src="../assets/hotel-3.jpg"
+        alt="hotel 1"
+        className="detail-imgs__img"
+      />
+    </div>
+
+    <div className="detail-main">
+      <h2 className="detail-main__title">Hotel las palmas</h2>
+      <span className="detail-main__location">Albufeira, Portugal</span>
+      <div className="detail-main__votes">
+        <h2 className="average">9.4</h2>
+        <span>234 Votes</span>
+      </div>
+    </div>
+  </div>
+);
 
 export default Details;
