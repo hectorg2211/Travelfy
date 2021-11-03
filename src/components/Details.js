29fad7fa0a08acd779b9841385f55b88a226c5c2
hectorg2211 <hector032716@gmail.com>
2021-11-03 07:13:08 -0700

    Added list and description styles

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { RiStarSFill, RiStarSLine } from "react-icons/ri";
 
 const Details = () => (
   <div className="details">
@@ -22,12 +23,46 @@ const Details = () => (
 
     <div className="detail-main">
       <h2 className="detail-main__title">Hotel las palmas</h2>
-      <div className="detail-main__stars">*****</div>
+      <div className="detail-main__stars">
+        <RiStarSFill />
+        <RiStarSFill />
+        <RiStarSFill />
+        <RiStarSFill />
+        <RiStarSLine />
+      </div>
       <span className="detail-main__location">Albufeira, Portugal</span>
       <div className="detail-main__votes">
         <h2 className="average">9.4</h2>
         <span className="count">234 Votes</span>
       </div>
+    </div>
+
+    <div className="detail-info">
+      <div className="description">
+        <p className="paragraph">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae
+          inventore maxime possimus aliquid dolor, debitis quasi id similique
+          quos esse repellendus consectetur quibusdam nesciunt molestiae eos
+          repellat tenetur minus officiis!
+        </p>
+        <p className="paragraph">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae
+          inventore maxime possimus aliquid dolor, debitis quasi id similique
+          quos.
+        </p>
+        <ul className="list">
+          <li className="list__item">Close to the beach</li>
+          <li className="list__item">Breakfast included</li>
+          <li className="list__item">Free airport shuttle</li>
+          <li className="list__item">Free wifi in all rooms</li>
+          <li className="list__item">Air conditioning and heat</li>
+          <li className="list__item">Pets allowed</li>
+          <li className="list__item">We speak all languages</li>
+          <li className="list__item">Perfect for families</li>
+        </ul>
+        <div className="recommend"></div>
+      </div>
+      <div className="user-reviews">User reviews</div>
     </div>
   </div>
 );
